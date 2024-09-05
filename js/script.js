7907a24a5e7528fccd3f04e144f4f37204f19da9
Muneeb-IW <muneebhaider-iw@gmail.com>
2024-09-05 11:42:17 +0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY) {
+            navbar.classList.add('bg-dark');
+        } else {
+            navbar.classList.remove('bg-dark');
+        }
+    })
+}
+document.addEventListener('DOMContentLoaded', userScroll);
